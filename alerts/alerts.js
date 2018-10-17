3d01346f4567e5a013b2bc6eeaa56d92996940e7
Lars <lars@k7c.dk>
2018-10-17 18:52:02 +0200

    tilføjet utc ur

--- alerts/alerts.js
+++ alerts/alerts.js
@@ -1,26 +1,48 @@
 $(document).ready(function() {
-  const HOURHAND = $("#hour");
-  const MINUTEHAND = $("#minute");
-  const SECONDHAND = $("#second");
+  function getPositions(utc) {
+    let date = new Date();
 
-  let date = new Date();
-  let hr = date.getHours();
-  let min = date.getMinutes();
-  let sec = date.getSeconds();
+    let hr = utc ? date.getUTCHours() : date.getHours();
+    let min = utc ? date.getUTCMinutes() : date.getMinutes();
+    let sec = utc ? date.getUTCSeconds() : date.getSeconds();
 
-  let hrPosition = (hr * 360) / 12 + (min * (360 / 60)) / 12;
-  let minPosition = (min * 360) / 60 + (sec * (360 / 60)) / 60;
-  let secPosition = (sec * 360) / 60;
+    let fullDate = utc
+      ? date.getUTCDate() + "." + date.getUTCMonth() + "." + date.getUTCFullYear()
+      : date.getDate() + "." + date.getMonth() + "." + date.getFullYear();
 
-  function runTheClock() {
-    hrPosition = hrPosition + 3 / 360;
-    minPosition = minPosition + 6 / 60;
-    secPosition = secPosition + 6;
+    return {
+      date: fullDate,
+      hrPosition: (hr * 360) / 12 + (min * (360 / 60)) / 12,
+      minPosition: (min * 360) / 60 + (sec * (360 / 60)) / 60,
+      secPosition: (sec * 360) / 60
+    };
+  }
+
+  function runTheClock(utc) {
+    let postfix = utc === true ? "-utc" : "";
+    let positions = getPositions(utc);
+
+    let HOURHAND = $("#hour" + postfix);
+    let MINUTEHAND = $("#minute" + postfix);
+    let SECONDHAND = $("#second" + postfix);
+
+    $("#date" + postfix).text(positions.date);
+
+    hrPosition = positions.hrPosition + 3 / 360;
+    minPosition = positions.minPosition + 6 / 60;
+    secPosition = positions.secPosition + 6;
 
     HOURHAND.css({ transform: "rotate(" + hrPosition + "deg)" });
     MINUTEHAND.css({ transform: "rotate(" + minPosition + "deg)" });
     SECONDHAND.css({ transform: "rotate(" + secPosition + "deg)" });
   }
 
-  setInterval(runTheClock, 1000);
+  function runTheClocks() {
+    runTheClock(false);
+    runTheClock(true);
+  }
+
+  (function() {
+    setInterval(runTheClocks, 1000);
+  })();
 });
